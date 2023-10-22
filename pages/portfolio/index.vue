22c2e238dc5273e27ee43ea461b77b68506d0d0d
Quentin Heinis <quentin@heinis.dev>
2023-10-22 21:55:45 +0200

    feat(prismic) : add projectList query + display data

--- pages/portfolio/index.vue
+++ pages/portfolio/index.vue
@@ -1,10 +1,40 @@
-<script setup></script>
+<script setup>
+const { client } = usePrismic();
+
+const { data: projects } = await useAsyncData("home", () => {
+  const response = client.getSingle("projectlist");
+  if (response) {
+    return response;
+  } else {
+    throw createError({ statusCode: 404, message: "Page not found" });
+  }
+});
+// console.log(projects.value);
+</script>
 
 <template>
-  <div>
+  <div class="p-portfolio">
     <h1>Projects</h1>
-    <NuxtLink to="portfolio/portfolio-eden-bourezg">Salut</NuxtLink>
+    <NuxtLink
+      class="p-portfolio__link"
+      v-for="(item, index) in projects.data.project_list"
+      :key="index"
+      :to="`portfolio/${item.project.uid}`"
+    >
+      <div>
+        <h2>{{ item.project_title[0].text }}</h2>
+        <img :src="item.project_img.url" alt="">
+        <p v-for="({text}, i) in item.project_stack" :key="i">{{ text }}</p>
+      </div>
+    </NuxtLink>
   </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.p-portfolio {
+  &__link {
+    color: $white;
+    text-decoration: none;
+  }
+}
+</style>
