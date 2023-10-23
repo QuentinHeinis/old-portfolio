151767ebd8e04d993fd16c3d2501c0fc04089d58
Quentin Heinis <quentin@heinis.dev>
2023-10-23 15:20:24 +0200

    feat(layout) : add scroll to top btn

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,36 +1,76 @@
 <script setup>
 const cursor = ref();
 const isInit = ref(false);
+const isScrolled = ref(false);
+const isHovered = ref(false);
+const scrollTop = ref();
 onMounted(() => {
   cursor.value.focus();
   isInit.value = true;
+
+  // add event listeners
   window.addEventListener("mousemove", positionElement);
   window.addEventListener("hovered", handleHover);
   window.addEventListener("leave", handleLeave);
+  window.addEventListener("scroll", handleScroll);
 });
 
-const isHovered = ref(false);
-const handleHover = () => {
-  isHovered.value = true;
-};
-const handleLeave = () => {
-  isHovered.value = false;
-};
 const positionElement = (e) => {
   const mouseY = e.clientY;
   const mouseX = e.clientX;
   cursor.value.style.top = `${mouseY}px`;
   cursor.value.style.left = `${mouseX}px`;
 };
+
+const handleHover = () => {
+  isHovered.value = true;
+};
+
+const handleLeave = () => {
+  isHovered.value = false;
+};
+
+const handleScroll = () => {
+  if (window.scrollY > 10) {
+    isScrolled.value = true;
+    scrollTop.value.style.animation = "pop-up 0.5s ease";
+  } else {
+    isScrolled.value = false;
+  }
+};
+
+const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
+
+// remove event listeners
 onUnmounted(() => {
   window.removeEventListener("mousemove", positionElement);
   window.removeEventListener("hovered", handleHover);
   window.removeEventListener("leave", handleLeave);
+  window.removeEventListener("scroll", handleScroll);
 });
 </script>
 
 <template>
-  <span ref="cursor" :class="{'cursor': isInit , ' -hovered': isHovered}"></span>
+  <span ref="cursor" :class="{ cursor: isInit, ' -hovered': isHovered }"></span>
+  <span class="scrollTop" @click="scrollToTop()" ref="scrollTop" v-if="isScrolled">
+    <span class="scrollTop__wrap">
+      <img src="/127.png" alt="" />
+      <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+        <circle id="curve" cx="50" cy="50" r="50" fill="none" />
+        <text width="500">
+          <textPath xlink:href="#curve">
+            Scroll to top &nbsp; - Scroll to top &nbsp;- Scroll to top -
+          </textPath>
+        </text>
+      </svg>
+    </span>
+  </span>
+
   <header class="header">
     <nav class="header__nav">
       <NuxtLink to="/" class="header__link">Quentin Heinis</NuxtLink>
@@ -46,10 +86,60 @@ onUnmounted(() => {
 </template>
 
 <style lang="scss">
+@keyframes pop-up {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.2);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
 body {
   color: $black;
   background: $white;
   cursor: none;
+  .scrollTop {
+    cursor: pointer;
+    height: rem(120);
+    width: rem(120);
+    position: fixed;
+    z-index: 150;
+    bottom: 5%;
+    right: 5%;
+    background: rgb(255, 89, 89);
+    border-radius: 100%;
+    &__wrap {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      height: 100%;
+      width: 100%;
+      position: relative;
+      img {
+        width: 50%;
+        height: 50%;
+      }
+      svg {
+        overflow: visible;
+        position: absolute;
+        top: 0;
+        left: 0;
+        scale: 0.75;
+        animation: rotate 10s linear infinite;
+      }
+      @keyframes rotate {
+        0% {
+          transform: rotate(0deg);
+        }
+        100% {
+          transform: rotate(360deg);
+        }
+      }
+    }
+  }
   .cursor {
     position: fixed;
     z-index: 1000;
