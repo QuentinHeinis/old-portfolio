2625cc22410b0d942329aa3737a2650691e912ff
Quentin Heinis <quentin@heinis.dev>
2023-10-23 12:51:02 +0200

    feat(index) : add images

--- pages/index.vue
+++ pages/index.vue
@@ -10,12 +10,14 @@ useSeoMeta({
 
 <template>
   <div class="p-index">
-    <span class="p-index__text">Hello</span>
-    <h1 class="p-index__text">Je suis Quentin Heinis</h1>
-    <p class="p-index__text -paragraphe">
-      Un développeur full-stack
-      <span class="p-index__text">basé en France</span>
+    <span class="p-index__text">Hello <img src="/8.png" alt="" class="p-index__img"></span>
+    
+    <h1 class="p-index__text">I am<img src="/1.png" alt="" class="p-index__img -space">Quentin Heinis</h1>
+    <p class="p-index__text">
+      A Full-stack <img src="/76.png" alt="" class="p-index__img">
     </p>
+    <p class="p-index__text">Developer</p> 
+    <p class="p-index__text">based in <img src="/48.png" alt="" class="p-index__img"> France</p>
 
     <div>
       
@@ -26,15 +28,20 @@ useSeoMeta({
 <style lang="scss">
 .p-index {
   &__text {
+    display: flex;
+    align-items: center;
+    text-wrap: nowrap;
     font-family: "Roboto", sans-serif;
     font-weight: 400;
     text-transform: uppercase;
     margin: 0;
     font-size: 8vw;
     letter-spacing: -5px;
-    &.-paragraphe {
-      display: flex;
-      flex-direction: column;
+  }
+  &__img{
+    max-height: 8vw;
+    &.-space{
+      padding: 0 2%;
     }
   }
 }
