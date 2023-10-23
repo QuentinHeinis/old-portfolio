ebcab9194c77e05ae3b14f8c869d2d4878f0fa39
Quentin Heinis <quentin@heinis.dev>
2023-10-23 12:32:51 +0200

    feat(page transition) : change blur to bouncy transition

--- app.vue
+++ app.vue
@@ -4,13 +4,21 @@
   </NuxtLayout>
 </template>
 <style lang="scss">
-.page-enter-active,
-.page-leave-active {
-  transition: all 0.4s;
+.page-enter-active {
+  animation: bounce-in 0.5s;
 }
-.page-enter-from,
-.page-leave-to {
-  opacity: 0;
-  filter: blur(1rem);
+.page-leave-active {
+  animation: bounce-in 0.5s reverse;
+}
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.1);
+  }
+  100% {
+    transform: scale(1);
+  }
 }
 </style>
