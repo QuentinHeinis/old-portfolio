49360429d99903711d1d131f4d970620fe53c351
Suihira <94958477+QuentinHeinis@users.noreply.github.com>
2024-06-13 14:15:27 +0200

    Update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -78,6 +78,8 @@ useSeoMeta({
       >
       in my free time.
     </p>
+    <p class="p-about__text -small">At Studio Pogo, I am using often AstroJs and if I need to I install
+<i class="-blue">Reactjs</i> or <i class="-green">Vuejs</i> on it</p>
     <p class="p-about__text -small">
       Now, my biggest project I'm working on is a game engine made in
       <i @mouseleave="emitLeaveHovered" @mouseover="emitHovered" class="-red"
